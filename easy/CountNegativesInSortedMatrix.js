e4f2b9f02d90b47c97cfa04c776a7977afd4d910
Jacob Vaanholt <Jacobvaanholt7@gmail.com>
2023-02-12 22:31:42 -0500

    completed problem

---
+++ easy/CountNegativesInSortedMatrix.js
@@ -0,0 +1,9 @@
+let negatives = 0;
+    for(let i=0; i < grid.length; i++){
+        for(let j=0; j< grid[i].length; j++){
+            if(grid[i][j] < 0){
+                negatives++;
+            }
+        }
+    }
+    return negatives;
