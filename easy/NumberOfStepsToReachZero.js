d19ba410eede1ceb0551833d6090d6e4ec69493c
Jacob Vaanholt <Jacobvaanholt7@gmail.com>
2023-02-12 23:07:30 -0500

    completed problem

---
+++ easy/NumberOfStepsToReachZero.js
@@ -0,0 +1,11 @@
+let steps = 0;
+    while(num != 0){
+        if(num % 2 == 0){
+            num /= 2;
+        }
+        else{
+            num -= 1;
+        }
+        steps++;
+    }
+    return steps;
